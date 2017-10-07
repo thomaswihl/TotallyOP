ac14d08a7a646143a67257ce416c405ea691c074
Thomas Wihl <thomas@speedi.local>
2017-10-07 10:24:47 +0200

    Added support for Extra Utilities Ender Lily and Red Orchid. Removing one seed from drops.

--- src/main/java/at/witho/totally_op/blocks/tileentity/TileFarmingFlower.java
+++ src/main/java/at/witho/totally_op/blocks/tileentity/TileFarmingFlower.java
@@ -2,17 +2,22 @@ package at.witho.totally_op.blocks.tileentity;
 
 import at.witho.totally_op.TotallyOP;
 import at.witho.totally_op.config.Config;
+import at.witho.totally_op.util.PrintClassHierarchy;
+import com.mojang.authlib.GameProfile;
 import net.minecraft.block.*;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.management.PlayerInteractionManager;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.IPlantable;
+import net.minecraftforge.common.util.FakePlayer;
 import org.apache.logging.log4j.Level;
 
+import java.util.List;
 import java.util.Random;
 
 public class TileFarmingFlower extends TileFunctionFlower {
@@ -34,16 +39,24 @@ public class TileFarmingFlower extends TileFunctionFlower {
         HarvestInfo info = canHarvest(state);
         if (info != null) {
             if (info.harvestPos == null) info.harvestPos = currentPos;
-            NonNullList<ItemStack> drops = NonNullList.create();
             Block block = state.getBlock();
-            block.getDrops(drops, world, info.harvestPos, state, 0);
+            NonNullList<ItemStack> drops = NonNullList.create();
+            // New way isn't supported by all
+            // block.getDrops(drops, world, info.harvestPos, state, 0);
+            // so use the old way:
+            drops.addAll(block.getDrops(world, currentPos, state, 0));
             world.setBlockToAir(info.harvestPos);
             boolean allIsSeed = info.seed != null;
+            boolean seedRemoved = false;
             for (ItemStack stack : drops) {
                 Item item = stack.getItem();
                 if (allIsSeed && !stack.isItemEqual(info.seed)) allIsSeed = false;
                 if ((info.seed == null || !stack.isItemEqual(info.seed)) && !info.ignoreFortune) {
                     stack.setCount(stack.getCount() * fortune);
+                }
+                if (!seedRemoved && stack.isItemEqual(info.seed)) {
+                    stack.setCount(stack.getCount() - 1);
+                    seedRemoved = true;
                 }
                 world.spawnEntity(new EntityItem(world, info.harvestPos.getX(), info.harvestPos.getY(), info.harvestPos.getZ(), stack));
             }
@@ -61,13 +74,15 @@ public class TileFarmingFlower extends TileFunctionFlower {
                 }
             }
 		}
-//		else {
-//            Block block = state.getBlock();
-//            if (block instanceof IGrowable) {
-//                IGrowable crop = (IGrowable)block;
-//                crop.grow(world, world.rand, currentPos, state);
-//            }
-//        }
+		else {
+            if (world.rand.nextFloat() < fortuneTier / 48.0) {
+                Block block = state.getBlock();
+                if (block instanceof IGrowable) {
+                    IGrowable crop = (IGrowable) block;
+                    crop.grow(world, world.rand, currentPos, state);
+                }
+            }
+        }
 
 		nextBlock();
 	}
@@ -90,6 +105,13 @@ public class TileFarmingFlower extends TileFunctionFlower {
             }
         } else if (block instanceof BlockMelon || block instanceof BlockPumpkin) {
             return new HarvestInfo();
+        } else if (block instanceof IGrowable) {
+            // cocoa beans
+            IGrowable growable = (IGrowable)block;
+            if (growable.canGrow(world, pos, state, false)) return null;
+            HarvestInfo info = new HarvestInfo();
+            info.seed = new ItemStack(block.getItemDropped(state, world.rand, 0));
+            return info;
         } else if (block instanceof IPlantable) {
             // cactus and sugar cane
             int size = 0;
@@ -97,18 +119,13 @@ public class TileFarmingFlower extends TileFunctionFlower {
             do {
                 pos = pos.up();
                 size++;
-            }   while (world.getBlockState(pos).getBlock() == block);
+            } while (world.getBlockState(pos).getBlock() == block);
             if (size > 1) {
                 HarvestInfo info = new HarvestInfo();
                 info.harvestPos = pos.down();
                 info.ignoreFortune = true;
                 return info;
             }
-        } else if (block instanceof IGrowable) {
-            // cocoa beans
-            IGrowable growable = (IGrowable)block;
-            if (growable.canGrow(world, pos, state, false)) return null;
-            return new HarvestInfo();
         }
 
         return null;
