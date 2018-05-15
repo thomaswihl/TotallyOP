20d07fbadf3a3bdf4bf19d82c29828568ef2c298
Thomas Wihl <thomas@speedi.local>
2018-05-15 19:09:23 +0200

    You can now have multiple Rucksack suck up items.

--- src/main/java/at/witho/totally_op/items/Rucksack.java
+++ src/main/java/at/witho/totally_op/items/Rucksack.java
@@ -29,6 +29,8 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Level;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Rucksack extends Item {
@@ -75,8 +77,8 @@ public class Rucksack extends Item {
     public void onItemPickup(EntityItemPickupEvent event) {
         if (event.isCanceled()) return;
         EntityPlayer player = event.getEntityPlayer();
-        ItemStack rucksack = getActiveRucksack(event.getEntityPlayer());
-        if (!rucksack.isEmpty()) {
+        List<ItemStack> rucksackList = getActiveRucksack(event.getEntityPlayer());
+        for (ItemStack rucksack : rucksackList) {
             RucksackStorage storage = new RucksackStorage(rucksack);
             ItemStack insert = event.getItem().getItem();
             ItemStack remain = storage.addItem(insert);
@@ -85,20 +87,24 @@ public class Rucksack extends Item {
                 World world = player.world;
                 world.playSound(null, player.posX, player.posY, player.posZ,
                         SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.PLAYERS, 0.2F, ((world.rand.nextFloat() - world.rand.nextFloat()) * 0.7F + 1.0F) * 2.0F);
-                if (remain.isEmpty() && event.isCancelable()) event.setCanceled(true);
+                if (remain.isEmpty()) {
+                    if (event.isCancelable()) event.setCanceled(true);
+                    return;
+                }
             }
         }
     }
 
-    public ItemStack getActiveRucksack(EntityPlayer player) {
+    public List<ItemStack> getActiveRucksack(EntityPlayer player) {
+        List<ItemStack> list = new ArrayList<>();
         InventoryPlayer inventory = player.inventory;
         for (ItemStack item : inventory.mainInventory) {
-            if (isActiveRucksack(item)) return item;
+            if (isActiveRucksack(item)) list.add(item);
         }
         for (ItemStack item : inventory.offHandInventory) {
-            if (isActiveRucksack(item)) return item;
+            if (isActiveRucksack(item)) list.add(item);
         }
-        return ItemStack.EMPTY;
+        return list;
     }
 
     public static boolean isActiveRucksack(ItemStack item) {
